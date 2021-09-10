13d7c0ba4dd0ed9686934874136fcfcc2b7adf71
Ayana VP <89035760+AyanaVP@users.noreply.github.com>
2021-09-10 20:47:22 +0530

    first change

---
+++ break.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Break function</title>
+</head>
+<body>
+
+	<?php
+
+	for($i=1; $i<10; $i++){
+	if($i==5)
+	{
+		break;
+	}
+	echo "The number is $i <br>";
+	}
+
+	?>
+
+
+</body>
+</html>
