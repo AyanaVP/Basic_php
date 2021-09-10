bdb064f494211a939a43a84e43c758ed23018912
Ayana VP <89035760+AyanaVP@users.noreply.github.com>
2021-09-10 20:47:51 +0530

    first change

---
+++ continue.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Continue</title>
+</head>
+<body>
+
+	<?php
+
+	for($i=1; $i<=10; $i++)
+	{
+		if($i==5)
+	{
+		continue;
+	}
+		echo "The number is $i <br>";	
+		
+	}
+
+	?>
+
+</body>
+</html>
